e10a96cb709a61bd5b0a825f9fefae470c58da83
Dmitry Shemetov <dshemetov@ucdavis.edu>
2025-01-22 14:33:15 -0800

    feat: first commit

---
+++ R/testbox.R
@@ -0,0 +1,11 @@
+#' Test function
+#'
+#' This is a test function.
+#'
+#' @param a A number.
+#' @param b A number.
+#' @return A number.
+#' @export
+test_function <- function(a = 1, b = 2) {
+  print("Hello, world!")
+}
